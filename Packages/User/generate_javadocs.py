36212e03bd63c64bbc2586f58bb6623d39ed2272
Spencer <foxtrotalphalima@gmail.com>
2017-11-29 13:31:06 -0500

    updates

--- Packages/User/generate_javadocs.py
+++
@@ -1,7 +0,0 @@
-import sublime
-import sublime_plugin
-from subprocess import call
-
-class GenerateJavadocs(sublime_plugin.WindowCommand):
-    def run(self):
-        call('/home/sbrow/Code/Java/generateDocs.sh');
--- Packages/User/generate_pydocs.py
+++
@@ -1,7 +0,0 @@
-import sublime
-import sublime_plugin
-from subprocess import call
-
-class GeneratePydocs(sublime_plugin.WindowCommand):
-    def run(self):
-        call('/home/sbrow/Code/Python/gen_py_docs.sh')
